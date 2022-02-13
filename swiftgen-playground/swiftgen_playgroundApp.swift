3697594b4bbaa3e4f8c90f5f4c5a4f93a12f124e
Mathilde Ferrand <mathilde.ferrand.coeurderoy@gmail.com>
2022-02-13 10:37:15 +0000

    chore: initial commit

---
+++ swiftgen-playground/swiftgen_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftgen_playgroundApp.swift
+//  swiftgen-playground
+//
+//  Created by Mathilde Ferrand on 13/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftgen_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
